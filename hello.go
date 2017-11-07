55edf88fac2baed4fd41a1882bb574f939234c78
gauravdesale <desalegaurav60@gmail.com>
2017-11-07 12:45:52 -0600

    lmao

---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+import (
+	"fmt"
+	"github.com/user/stringutil"
+)//lets import that library
+func main() {
+	fmt.Println("hello, world\n");
+	fmt.Println(stringutil.Reverse("!oG, olleH"))//we are using the package we made to reverse both those strings and print them again
+}
